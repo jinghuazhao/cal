2ba7a02d3858f5249ac2e1a408c55c59008406a8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-08-25 08:09:37 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,8 @@
+git init
+git add Game.org
+git commit -m "Game"
+git branch -M main
+git add st.sh
+git commit -m "st.sh"
+git remote add origin git@github.com:jinghuazhao/cal.git
+git push -u origin main
